c01bbefa588576a00832160be896773756b463ff
Agbele_Kolade <kolagbele@gmail.com>
2020-06-12 13:57:00 +0200

    ADD: Header styling updated

--- src/components/layouts/appLayout/header/Logo.js
+++ src/components/layouts/appLayout/header/Logo.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-//import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import brand from '../../../../assets/brand.png';
+import Drawer from './Drawer';
+
+const Wrapper = styled.div`
+	display: grid;
+	grid-template-columns: max-content max-content;
+	grid-gap: 1rem;
+`;
 
 function Logo() {
 	return (
-		<div>
+		<Wrapper>
+			<Drawer />
 			<Link to="/" className="nav-title">
 				<img src={brand} alt="logo" />
 			</Link>
-		</div>
+		</Wrapper>
 	);
 }
 
--- src/components/layouts/appLayout/header/index.js
+++ src/components/layouts/appLayout/header/index.js
@@ -9,89 +9,100 @@ import Container from '../../../common/Container';
 import Logo from './Logo';
 
 
+const Wrapper = styled.nav`
+	height: max-content;
+	width: 100%;
+	background: rgba(203, 243, 240, 0.1);
+	border: 1px solid rgba(0, 0, 0, 0.1);
+	padding: 0.5rem 0rem;
+	.nav {
+		width: 100%;
+		display: grid;
+		grid-template-columns: max-content 1fr max-content;
+		justify-content: space-between;
+		align-items: center;
+		grid-gap: 1rem;
+	}
+	.middle-nav{
+		display: flex;
+		justify-content: center;
+	}
+	.nav-links {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		min-width: 35rem;
+	}
+	.nav-buttons {
+	 display: grid;
+	 grid-gap: 1rem;
+	 grid-template-columns: max-content max-content;
+	}
+	@media screen and ( max-width: ${props => props.theme.breakpoint.md}) {
+		.middle-nav, .nav-buttons {
+			display: none;
+		}
+		.nav {
+			display: flex;
+			justify-content: center;
+		}
+	}
+`;
+
+const NavLinks = styled(NavLink)`
+	display: inline-block;
+	text-decoration: none;
+	font-style: normal;
+	font-weight: 500;
+	font-size: 14px;
+	line-height: 25px;
+	color: #1d1d1d;
+	max-width: 9rem;
+	text-align: center;
+	&:hover {
+		color: #fcb813;
+		 transition: 0.2s;
+	};
+`;
+
+const buttonStyles = {
+	minWidth : '8rem',
+	maxWidth : '10rem',
+	paddingLeft: '1rem',
+	paddingRight: '1rem',
+};
+
 export default function Header() {
 	return (
-		<Nav>
+		<Wrapper>
 			<Container>
-				<div>
-					<Logo />
-				</div>
+				<div className="nav">
 
-				<NavLinksAll className="nav-links flex">
-					<div style={{ padding: '0 50px' }}>
-						<NavLinks exact activeClassName="navbar__link--active" to="/">
-            Home
-						</NavLinks>
-						<NavLinks activeClassName="navbar__link--active" to="/about">
-            About Us
-						</NavLinks>
-						<NavLinks activeClassName="navbar__link--active" to="/wellness">
-            Wellness
-						</NavLinks>
-						<NavLinks activeClassName="navbar__link--active" to="/fitnessFair">
-            Fitness Fair
-						</NavLinks>
-						<NavLinks activeClassName="navbar__link--active" to="/contactUs">
-            Contact Us
-						</NavLinks>
+					<Logo />
+
+					<div className="middle-nav">
+						<div className="nav-links">
+							<NavLinks exact activeClassName="navbar__link--active" to="/"> Home </NavLinks>
+							<NavLinks activeClassName="navbar__link--active" to="/about">About Us</NavLinks>
+							<NavLinks activeClassName="navbar__link--active" to="/wellness">Wellness</NavLinks>
+							<NavLinks activeClassName="navbar__link--active" to="/fitnessFair">Fitness Fair</NavLinks>
+							<NavLinks activeClassName="navbar__link--active" to="/contactUs">Contact Us</NavLinks>
+						</div>
 					</div>
+
 					<div className="nav-buttons">
-						<Button value="Log In" theme="green" style={{ marginRight: '18px' }}>
-							{' '}
-            Log In
-						</Button>
 						<Link to="/signUp">
-							<Button value="Join Us" theme="yellow">
-								{' '}
-              Join Us
-							</Button>
+							<Button value="Log In" theme="green" style={buttonStyles}>Log In</Button>
+						</Link>
+						<Link to="/signUp">
+							<Button value="Join Us" theme="yellow" style={buttonStyles}>Join Us</Button>
 						</Link>
 					</div>
-				</NavLinksAll>
+				</div>
 			</Container>
-		</Nav>
+		</Wrapper>
 	);
 }
 
-const Nav = styled.nav`
-  height: max-content;
-  width: 100%;
-  background: rgba(203, 243, 240, 0.1);
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  padding: 0.5rem 0rem;
-`;
 
-const breakPoint = '1000px';
-
-
-const NavLinks = styled(NavLink)`
-display: inline-block;
-  padding: 13px 32px;
-  font-family: cursive;
-  text-decoration: none;
-  /* text-transform: lowercase; */
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 25px;
-  color: #1d1d1d;
-  &:hover {
-    color: #fcb813;
-  transition: 0.2s;
-  };
-`;
-
-
-const NavLinksAll = styled.div`
-  @media (max-width: ${breakPoint}) {
-    position: absolute;
-    display: block;
-    width: 100%;
-    background-color: #333;
-    height: 0px;
-    transition: all 0.3s ease-in;
-    overflow-y: hidden;
-    top: 50px;
-    left: 0px;
-  }`;
 
