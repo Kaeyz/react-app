f070d68d7bb7e9aab1f9c9179356d1b1b74b1ffa
Ayokanmi Damilola <dammieayokanmi@gmail.com>
2020-06-15 15:43:34 +0100

    ADD: started DoYouKnow and HealthTrack components

--- src/components/wellness/individual/DoYouKnow.js
+++ src/components/wellness/individual/DoYouKnow.js
@@ -1,28 +1,85 @@
-import React from 'react';
-import Container from '../../common/Container';
-import styled from 'styled-components';
-
-//import PropTypes from 'prop-types';
+// modules
+import React from "react";
+import styled from "styled-components";
+import Container from "../../common/Container";
+import { Link } from "react-router-dom";
+import arrow from "../../../assets/Arrow1.png";
+import img from "../../../assets/undraw_medicine.svg";
 
 const Wrapper = styled.div`
 
-
+  .flexy {
+    flex-wrap: wrap;
+    justify-content: space-between;
+	align-items: center;
+	padding: 142px 0;
+	
+    .leadInfo {
+      flex-direction: column;
+      align-items: end;
+      flex-direction: column-reverse;
+      height: 32.7rem;
+      max-width: 100%;
+	  justify-content: space-between;
+	  
+      .linkSpan1 {
+        font-size: 4.1rem;
+        line-height: 6.4rem;
+        color: ${(props) => props.theme.color.text_01};
+        text-decoration-line: underline;
+        margin-right: 2.5rem;
+      }
+    }
+  }
+  .leaderBg {
+    background-image: url(${img});
+    width: 38.9rem;
+    height: 27.6rem;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  h1 {
+    width: 52.7rem;
+    max-width: 100%;
+    font-size: 72px;
+    line-height: 86px;
+    color: ${(props) => props.theme.color.text_01};
+    margin-bottom: 2.9rem;
+  }
+  p {
+    font-size: 14px;
+line-height: 24px;
+    text-transform: uppercase;
+    color: ${(props) => props.theme.color.text_02};
+  }
 `;
 
-function DoYouKnow() {
-	return (
-		<Wrapper>
-			<Container>
-				{/* Code goes here */}
-				DoYouKnow
-			</Container>
-		</Wrapper>
-	);
+export default function DoYouKnow() {
+  return (
+    <Wrapper>
+      <Container>
+        <div className="flexy flex">
+          <div className="flex leadInfo">
+            <div style={{ maxWidth: "100%" }}>
+              <h1>Did you know?</h1>
+              <p>A lack of exercise now causes as many deaths as smoking.</p>
+            </div>
+            <div>
+              <Link to="../pages/About.js">
+                <span className="linkSpan1">See Fitness</span>
+                <span>
+                  {" "}
+                  <img src={arrow} alt="arrow" className="arrow" />
+                </span>{" "}
+              </Link>
+            </div>
+          </div>
+          <div className="leaderBg"></div>
+        </div>
+      </Container>
+    </Wrapper>
+  );
 }
 
-DoYouKnow.propTypes = {
-
-};
-
-export default DoYouKnow;
-
+DoYouKnow.propTypes = {};
--- src/components/wellness/individual/HealthTrackCard.js
+++ src/components/wellness/individual/HealthTrackCard.js
@@ -1,27 +1,71 @@
-import React from 'react';
-import Container from '../../common/Container';
-import styled from 'styled-components';
-
+import React from "react";
+import Container from "../../common/Container";
+import styled from "styled-components";
+import Button from "../../common/Button";
+import trackImg from "../../../assets/nutrition-rs.png";
 //import PropTypes from 'prop-types';
 
 const Wrapper = styled.div`
-
-
+  background: #cbf3f0;
+  padding: 4.3rem 0;
+  .tracker {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+  .trackInfo {
+    margin: 2rem 0;
+    font-size: 14px;
+    line-height: 24px;
+    color: ${(props) => props.theme.color.text_02};
+  }
+  .detailImg {
+    min-width: 30rem;
+    max-width: 34rem;
+    margin-left: 3rem;
+    margin-top: 3rem;
+  }
+  .details {
+    width: 55%;
+  }
+  h3 {
+    font-size: 30px;
+    line-height: 32px;
+    color: ${(props) => props.theme.color.text_01};
+  }
 `;
 
 function HealthTrackCard() {
-	return (
-		<Wrapper>
-			<Container>
-				{/* Code goes here */}
-				HealthTrackCard
-			</Container>
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      <Container>
+        <div className="tracker" style={{ flexDirection: "row-reverse" }}>
+          <div className="detailImg">
+            <img src={trackImg} alt="trackImg" />
+          </div>
+          <div className="details">
+            <h3>We track your health </h3>
+            <p className="trackInfo">
+              Are you stressed? Are you unhappy? How can we make your life or
+              you more productive and satisfied? Are you living a life of
+              balance and good adjustment? We ask the questions that help us
+              make you happier, healthier, and simply, the best version of
+              yourself.
+            </p>
+
+            <div>
+              <Button value="Get started" theme="darkGreen">
+                Get started
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </Wrapper>
+  );
 }
 
-HealthTrackCard.propTypes = {
-
-};
+HealthTrackCard.propTypes = {};
 
 export default HealthTrackCard;
