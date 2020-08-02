666b8a93b27a00eceb67e5242488adb5e57e37be
Agbele_Kolade <kolagbele@gmail.com>
2020-08-02 17:38:41 +0200

    ADD: updated

--- src/views/dashboard/assessment/hra_questionnaire/HealthReview.js
+++ src/views/dashboard/assessment/hra_questionnaire/HealthReview.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
 //import PropTypes from 'prop-types';
 import DashboardLayout from '../../../../components/layouts/dashboardLayout/DashboardLayout';
 import QuestionnaireLayout from '../../../../components/dashboard/assessment/hra_questionnaire/QuestionnaireLayout';
 import ReviewCard from '../../../../components/dashboard/assessment/hra_questionnaire/ReviewCard';
 
 
-const Wrapper = styled.div`
-
-`;
-
 function HealthReview() {
 
 	return (
 		<DashboardLayout>
 			<QuestionnaireLayout>
-				<Wrapper>
-					<ReviewCard />
-					<ReviewCard />
-					<ReviewCard />
-				</Wrapper>
+				<ReviewCard />
+				<ReviewCard />
+				<ReviewCard />
 			</QuestionnaireLayout>
 		</DashboardLayout>
 	);
