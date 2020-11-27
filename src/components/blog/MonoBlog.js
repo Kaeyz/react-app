9d73b28fafcc53745a966d2b421cfc934bb99562
Ayokanmi Damilola <dammieayokanmi@gmail.com>
2020-11-27 08:26:18 +0100

    ADD: connecting blogs to back

--- src/components/blog/MonoBlog.js
+++ src/components/blog/MonoBlog.js
@@ -3,10 +3,9 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import Button from "../common/Button";
-import blogImg from "../../assets/girlRunning.png";
 import { Card, CardContent } from "@material-ui/core";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 
 const Wrapper = styled.div`
@@ -15,6 +14,7 @@ const Wrapper = styled.div`
     min-width: 27rem;
     max-width: 30rem;
     margin: 0 0 3rem 0;
+    height:350px;
     border: 1px solid ${(props) => props.theme.color.ui_text_09};
     &:hover{
       box-shadow: 0px 3px 5px -1px rgba(0,0,0,0.2), 0px 5px 8px 0px rgba(0,0,0,0.14), 0px 1px 14px 0px rgba(0,0,0,0.12);
@@ -43,6 +43,15 @@ const Wrapper = styled.div`
   #yellow {
     background-color: rgba(247, 194, 54, 0.8);
   }
+  #blue{
+    background-color: rgba(46,196,182, 0.8);
+  }
+  #green{
+    background-color: rgba(141,184,56, 0.8);
+  }
+  #orange{
+    background-color: rgba(244,120,3, 0.8);
+  }
   .blog-title {
     color: ${(props) => props.theme.color.text_13};
     font-size: 1.6rem;
@@ -60,27 +69,28 @@ const Wrapper = styled.div`
 `;
 
  function MonoBlog({
-  getBlogs,
-  isLoading,
-  src,
-  id,
-  title,
+  author,
+  to,
+  title,tag,
     createdAt
+    ,src,alt,isLoading,tagColor
 }) {
 
   return (
     <Wrapper>
    
-      <Link class='mono-blog-link'>
+      <Link class='mono-blog-link' to={to}>
       <Card className="blog_card" elevation={0}>
         <div className="img_div">
-          <img src={blogImg} alt="blog img" className="blog_img" />
-          <p className="tag blog-tag" id="yellow">
-            Fitness
+      {  isLoading ? (
+      <div>Loading ...</div>) :
+          (<img src={src} alt={alt} className="blog_img" />)}
+          <p className="tag blog-tag" id={`${tagColor}`}>
+            {tag}
           </p>
         </div>
         <CardContent className={`card_content`}>
-          <h2 className="auto light">{id}</h2>
+          <h2 className="auto light">{author}</h2>
           <p className="blog-title">{title}</p>
           <h2 className="date light">{createdAt}</h2>
         </CardContent>
@@ -93,15 +103,24 @@ const Wrapper = styled.div`
 MonoBlog.defaultProps = {};
 
 MonoBlog.propTypes = {
- 
+  isLoading: PropTypes.bool.isRequired,
 	src: PropTypes.string,
-	alt: PropTypes.string,
 	id: PropTypes.string,
 	title: PropTypes.string.isRequired,
-	body: PropTypes.string.isRequired,
-	asset: PropTypes.any,
+	alt: PropTypes.string.isRequired,
+	author: PropTypes.string.isRequired,
+	tag: PropTypes.string.isRequired,
+	tagColor: PropTypes.string.isRequired,
+	to: PropTypes.any,
 };
 
 
 
-export default MonoBlog;
+const mapStateToProps = (state) => {
+	const { isLoading } = state.blog;
+	return { isLoading };
+};
+
+export default connect(mapStateToProps)(MonoBlog);
+
+
